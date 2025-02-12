37c66f142d29786f0b58c0881b35a494a0abe84a
N.,T. <127326243+TakuNagata@users.noreply.github.com>
2025-02-12 17:42:20 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/5hanayome-cardgame-counter/',
   plugins: [
     react(),
     VitePWA({
