80dce14cc36c51ffc7d8924cb91a6b444f4bdcd5
N.,T. <127326243+TakuNagata@users.noreply.github.com>
2025-02-16 00:33:15 +0900

    create deverop-v2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,29 +25,30 @@ const Hero = () => {
             />
         )
     })
-    
-    const selectArea = selectedApproach ? ( // アプローチ選択モード
-        <div
-            className="z-10 w-full h-full flex justify-center items-center appearance-none bg-transparent"
-            onClick={() => {
-                setSelectedApproach(null)
-                toggleBooleanAtIndex(selectedApproach, setIsApproachState)
-            }}
-        >
-            {requiredHanayomePower}
-        </div>) : (
-        <select
-            className="z-10 w-full h-full text-center appearance-none bg-transparent"
-            value={requiredHanayomePower}
-            onChange={(e) => setRequiredHanayomePower(Number(e.target.value))}
-        >
-            {[...Array(5)].map((_, i) => (
-                <option key={i} value={i + 6}>{i + 6}</option>
-            ))}
-            <option value={12}>{12}</option>
-            <option value={17}>{17}</option>
-        </select>
-    );
+
+    const selectArea =
+        selectedApproach ? ( // アプローチ選択モード
+            <div
+                className="z-10 w-full h-full flex justify-center items-center appearance-none bg-transparent"
+                onClick={() => {
+                    setSelectedApproach(null)
+                    toggleBooleanAtIndex(selectedApproach, setIsApproachState)
+                }}
+            >
+                {requiredHanayomePower}
+            </div>) : (
+            <select
+                className="z-10 w-full h-full text-center appearance-none bg-transparent"
+                value={requiredHanayomePower}
+                onChange={(e) => setRequiredHanayomePower(Number(e.target.value))}
+            >
+                {[...Array(5)].map((_, i) => (
+                    <option key={i} value={i + 6}>{i + 6}</option>
+                ))}
+                <option value={12}>{12}</option>
+                <option value={17}>{17}</option>
+            </select>
+        );
 
     return (
         <div className='Hero flex relative w-full h-full font-bold'>
